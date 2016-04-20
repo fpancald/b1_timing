98cfe1502b67782a062258b2550ec47c0e77d6ed
fpancald <fpancald@nd.edu>
2016-04-20 11:41:40 -0400

    added bertini2 and eigen dependency

--- include/b1_timing.hpp
+++ include/b1_timing.hpp
@@ -2,8 +2,9 @@
 #ifndef BERTINI1_TIMING_HPP
 #define BERTINI1_TIMING_HPP
 #include <cstddef>
-
 #include <chrono>
+
+#include"bertini2/eigen_extensions.hpp"
 
 #include "bertini_headers.hpp"
 
--- src/b1_timing.cpp
+++ src/b1_timing.cpp
@@ -5,7 +5,7 @@ int main()
     std::chrono::time_point<std::chrono::high_resolution_clock> ct1, ct2;
     std::chrono::duration<double> elapsed_seconds;
     FILE *fpA, *fpx, *fpb;
-		int prec=1024, size=100, iter=10000;
+		int prec=1024, size=100, iter=10;
     double tol=1e-16, largeChange=1e16;
     mpf_t mpftol,  mpflargeChange;
     mpf_init2(mpftol,prec);
@@ -28,12 +28,12 @@ int main()
 		make_matrix_random_mp(A,size, size, prec);
 		make_vec_random_mp(b,size);
 
-		fpA = fopen ("matrixA.txt" , "a+");
-		printMat_Matlab_mp(fpA, prec, A);
-    fclose(fpA);
-    fpb = fopen ("vectorb.txt" , "a+");
-		printVec_Matlab_mp(fpb, prec, b);
-    fclose(fpb);
+		// fpA = fopen ("matrixA.txt" , "a+");
+		// printMat_Matlab_mp(fpA, prec, A);
+    // fclose(fpA);
+    // fpb = fopen ("vectorb.txt" , "a+");
+		// printVec_Matlab_mp(fpb, prec, b);
+    // fclose(fpb);
 
     ct1=std::chrono::system_clock::now();
 
@@ -49,9 +49,10 @@ int main()
     ct2=std::chrono::system_clock::now();
     elapsed_seconds += ct2-ct1;
 
-    fpx = fopen ("vectorx.txt" , "a+");
-		printVec_Matlab_mp(fpx, prec, x);
-    fclose(fpx);
+    // fpx = fopen ("vectorx.txt" , "a+");
+		// printVec_Matlab_mp(fpx, prec, x);
+    // fclose(fpx);
+
 		clear_mat_mp(A);
     clear_vec_mp(x);
     clear_vec_mp(b);
